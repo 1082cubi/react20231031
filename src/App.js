b91ad8377955c5b2e28a0703124a13a574db4cb9
1082cubi <1082cubi@hanmail.net>
2023-10-31 16:37:45 +0900

    스타일 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
 function App() {
   return (
-    <div >
-
+    <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+      <MyComp />
+      <MyComp />
+      <MyComp />
     </div>
+  );
+}
+function MyComp() {
+  const name = "흥민";
+  return (
+    <>
+      <div style={{ backgroundColor: "blue", color: "yellow" }}>{name}</div>;
+    </>
   );
 }
 
