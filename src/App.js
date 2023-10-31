551d78309267dc204647abd05f621ae4b0d8df38
1082cubi <1082cubi@hanmail.net>
2023-10-31 16:11:57 +0900

    프로퍼티의 값은 자바스크트립트 코드가 될 수 있음

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 function App() {
-  // jsx가 js 코드로 변환됨
-  // 따라서 js 키워드를 사용 할 수 없음
+  const num1 = Math.ceil(Math.random() * 6);
+  const num2 = Math.ceil(Math.random() * 6);
+
+  let messageClassName = "lose";
+  let message = "다시던져보세요";
+
+  if (num1 === num2) {
+    messageClassName = "win";
+    message = "당첨";
+  }
+
   return (
     <>
-      {/*  class 속성은 className으로 작성*/}
-      <div className="header">lorem ipsum</div>
-      <div className="error">lorem ipsum</div>
-      {/*for 속성은 htmlFor로 작성 */}
-      <div>
-        <label htmlFor="nameInput">이름</label>
-        <input type="text" id="nameInput" />
-      </div>
-      <div>
-        <label htmlFor="ageInput">이름</label>
-        <input type="text" id="ageInput" />
-      </div>
-      {/*  */}
-      <div className={}></div>
+      <h1>1번 주사위{num1}</h1>
+      <h1>2번 주사위{num2}</h1>
+
+      <h1 className={messageClassName}>{message}</h1>
     </>
   );
 }
 
-function BigImage() {
-  return <h1>큰 이미지</h1>;
-}
-function SmallImage() {
-  return <h1>작은 이미지</h1>;
-}
 export default App;
