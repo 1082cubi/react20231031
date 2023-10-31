73cbbb1917236c8b2a0b44a5d89b3b06ab1557d7
1082cubi <1082cubi@hanmail.net>
2023-10-31 14:41:46 +0900

    jsx에서 js 코드 작성은 {} 사용

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
+function MYComp2() {
+  let name = "손흥민";
+  let age = 33;
+
+  // {} jsx에서 js 코드 작성하기 위한 기호
+  return (
+    <>
+      <h1>hello react</h1>
+      <h2>hello {name}</h2>
+      <h1>{age} years</h1>
+      <h1>{age * 2} double years</h1>
+      <h1>{name + "선수"}</h1>
+      {/* 그래서 주석이 이렇게 생김 */}
+    </>
+  );
+}
+
 function App() {
   // jsx에서 태그 사용시 꼭 종료태그를 작성해야 함
   // 빈 요소이면 시작태그에서 종료
@@ -11,6 +28,7 @@ function App() {
         lorem ipum dolr.
       </p>
       <MyComp />
+      <MYComp2 />
     </div>
   );
 }
