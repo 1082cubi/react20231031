589fa5b0843bca136df36ea1b7c129c63ca20da7
1082cubi <1082cubi@hanmail.net>
2023-10-31 15:24:20 +0900

    jsx의 {}에서 간단한 연산자 활용

--- src/App.js
+++ src/App.js
@@ -6,21 +6,17 @@ function App() {
     <>
       <h1>난수 : {number}</h1>
       <h1>{number > 50 ? "큰수" : "작은 수"}</h1>
-      <div>{number > 50 ? <BigImage /> : <smallImage />}</div>
+      <div>{number > 50 ? <BigImage /> : <SmallImage />}</div>
       <div>{isLarge && <h1>큰 수다 </h1>}</div>
       <div>{isLarge || <h1>작은 수다 </h1>}</div>
     </>
   );
 }
 
-// built-in component는 소문자로 시작
-// component 명은 꼭 대문자로 시작
-// return 문을 가져야함
-// return 값은 보통 JSX 코드
 function BigImage() {
   return <h1>큰 이미지</h1>;
 }
-function smallImage() {
-  return <h3></h3>;
+function SmallImage() {
+  return <h1>작은 이미지</h1>;
 }
 export default App;
