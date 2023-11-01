56eb43486002ede300c0109fccc969cd75781adb
1082cubi <1082cubi@hanmail.net>
2023-11-01 16:35:46 +0900

    css module을 사용할 때 여러 클래스명을 주는 방법

--- src/compornent/Mycontainer.js
+++ src/compornent/Mycontainer.js
@@ -4,6 +4,9 @@ export function Mycontainer() {
   return (
     <div>
       <h1 className="bosClass">lorem ipsum</h1>
+      <h1 className={[style.boxClass, style.error, style.note].join}>
+        lorem ipsum dolor
+      </h1>
     </div>
   );
 }
