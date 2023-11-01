e9ea698355c28d9c66dcd9237069146d58a0d21f
1082cubi <1082cubi@hanmail.net>
2023-11-01 11:48:13 +0900

    component를 다른파일에 작성 후 inport,export 하기

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
+// export된 컴포넌트(변수, 함수, 객체) 를 import 해서 사용
+import { MyElem } from "./component/MyElem";
+import { MyComp } from "./component/MyComp";
+
 function App() {
   return (
     <>
-      <MyComp name={"흥민"} address={"신촌"} age={33} city={"서울"} />
-      <MyComp address={"강남"} age={322} city={"부산"} />
-      <MyComp address={"런던"} city={"영국"} />
-      <MyComp name="희찬" age={24} city={"파리"} />
+      <MyComp />
+      <MyElem />
     </>
-  );
-}
-
-function MyComp({ name = "anonymous", address, city, age = 100 }) {
-  return (
-    <div>
-      <p>
-        {name} 온 {age}세 이고 {address}, {city}에 산다
-      </p>
-    </div>
   );
 }
 
