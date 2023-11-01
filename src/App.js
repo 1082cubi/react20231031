7dadc4a4b44998a49904fcdc889605f9e187893d
1082cubi <1082cubi@hanmail.net>
2023-11-01 15:48:00 +0900

    보통의 css 사용시 클래스명이 중복될 수 있음

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 // chakraui css library
 import { MyBox } from "./compornent/MyBox";
+import { Mycontainer } from "./compornent/Mycontainer";
+import { MyComp } from "./compornent/MyComp";
 
 function App() {
   return (
     <>
       <MyComp />
       <MyBox />
+      <Mycontainer />
     </>
   );
 }
---
+++ src/compornent/Mycontainer.js
@@ -0,0 +1,9 @@
+import "../style/style2.css";
+
+export function Mycontainer() {
+  return (
+    <div>
+      <h1 className="bosClass">lorem ipsum</h1>
+    </div>
+  );
+}
