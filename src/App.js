1f4d8791075833150acf056a061b48bac07ced8a
1082cubi <1082cubi@hanmail.net>
2023-11-02 11:10:51 +0900

    패딩,마진 적용

--- src/App.js
+++ src/App.js
@@ -3,7 +3,91 @@ import { Box } from "@chakra-ui/react";
 function App() {
   return (
     <>
+      <Box bg={"gold"} border={"1px slid purple"}>
+        Lorem ipsum.
+      </Box>
+      <Box bg={"blue"} border={"2px dotted green"}>
+        Exercitationem, fugit?
+      </Box>
+      <Box bg={"red"} borderBottom={"7px double yellow"}>
+        Dignissimos, optio.
+      </Box>
+      <Box borderX={"10px solid"} borderColor={"gray.600"}>
+        Ipsum, soluta.
+      </Box>
+      <Box borderY={"10px dotted"} borderColor={"red"}>
+        Quae, voluptate.
+      </Box>
+      <Box border={"5px solid blue"} borderRadius={"30px"}>
+        Autem, illum.
+      </Box>
+      <Box border={"5px solid yellow"} borderRadius={"20px"}>
+        Cum, laboriosam?
+      </Box>
+      <Box border={"5px solid black"} borderRadius={1}>
+        Corporis, saepe.
+      </Box>
+      <Box border={"5px solid blue"} borderTopRightRadius={10}>
+        Laborum, quo.
+      </Box>
+      <Box border={"5px solid green"} borderTopRightRadius={10}>
+        Dolorum, qui.
+      </Box>
+      <Box border={"5px solid purple"} borderTopRightRadius={10}>
+        Qui, voluptate?
+      </Box>
+      <Box border={"5px solid red"} borderTopLeftRadius={10}>
+        Consequuntur, sequi.
+      </Box>
+      <Box shadow={"10px 5px 10px"}>Labore, temporibus.</Box>
+      <Box shadow={"lg"}>Expedita, quam.</Box>
+      <Box shadow="xl">Consectetur, repudiandae?</Box>
+      <Box bg={"blue.300"} m={"10px"}>
+        A, debitis!
+      </Box>
+      <Box bg={"yellow.600"} m={"20px"}>
+        Blanditiis, eveniet?
+      </Box>
+      <Box bg={"facebook.500"} mx={"30px"}>
+        Fuga, omnis?
+      </Box>
+      <Box bg="red.600" m="1">
+        ksfdha
+      </Box>
+      <Box bg="blue.600" m="2">
+        ajkfhakjdhkj
+      </Box>
+      <Box bg="" m="">
+        alkjfdlajdlkf
+      </Box>
+      <Box bg="" m="">
+        adsjfl;kjdl
+      </Box>
+      <Box bg="" m="">
+        sadf
+      </Box>
+      <Box bg="red.400" p={"10px"}>
+        Lorem ipsum.
+      </Box>
+      <Box bg="blue.400" px={"200px"}>
+        Enim, labore!
+      </Box>
+      <Box bg="purple" py={"30px"}>
+        Accusamus, earum.
+      </Box>
+      <Box bg="gold" pb={"50px"}>
+        Sapiente, ut.
+      </Box>
+      <Box bg="tomato">Quod, voluptates!</Box>
+      <Box bg="red.600">Aut, vel!</Box>
+      <Box bg="blue.600">Veritatis, voluptatem!</Box>
+      <Box bg="gray.300">Iure, quas?</Box>
+      <Box bg="">Iure, pariatur.</Box>
+      <Box bg="">Cupiditate, quaerat?</Box>
 
+      <div style={{ margin: "10px", backgroundColor: "gold", color: "gray" }}>
+        lorem ipum.
+      </div>
     </>
   );
 }
