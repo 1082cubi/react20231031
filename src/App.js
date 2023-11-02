e31051f33c4bcc7c0f3a23adeded0c042cd8d3fa
1082cubi <1082cubi@hanmail.net>
2023-11-02 10:23:35 +0900

    필요없는 코드 삭제함

--- src/App.js
+++ src/App.js
@@ -3,21 +3,7 @@ import { Box } from "@chakra-ui/react";
 function App() {
   return (
     <>
-      <Box bgColor="tomato">Lorem ipsum dolor.</Box>
-      <Box bgColor="red">Lorem ipsum dolor.</Box>
-      <Box bgColor="blue">Eligendi, recusandae vitae.</Box>
-      <Box bgColor="blue.50">Eveniet, laborum minus!</Box>
-      <Box bgColor="blue.100">Debitis qui, quia.</Box>
-      <Box bgColor="blue.900">Aspernatur nam, voluptates!</Box>
-      <Box bgColor="gray">Autem, possimus, reiciendis?</Box>
-      <Box bgColor="gray.100">Beatae, deleniti, molestias?</Box>
-      <Box bgColor="gray.800">Maxime, placeat saepe?</Box>
-      <Box bg="yello.700" color="gray.200">
-        Fuga natus, rem.4
-      </Box>
-      <Box bg="facebook.500" color="linkedin.100">
-        Enim, libero possimus!
-      </Box>
+
     </>
   );
 }
