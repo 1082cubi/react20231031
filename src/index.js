30738c5b19825bf185e82b8c190c773978a42e59
1082cubi <1082cubi@hanmail.net>
2023-11-02 15:29:43 +0900

    Select 사용

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { Box, FormControl, FormLabel, Textarea } from "@chakra-ui/react";
 
+function App(props) {
+  return (
+    <div>
+      <Box w="480px">
+        <FormControl>
+          <FormLabel>본문</FormLabel>
+          <Textarea placeholder="본문을 작성하세요"></Textarea>
+        </FormControl>
+      </Box>
+    </div>
+  );
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+export default App;
