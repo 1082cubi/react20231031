5d1755deb97d60a32644a7e697db8b6d78617bc6
1082cubi <1082cubi@hanmail.net>
2023-11-02 14:13:44 +0900

    Modal 사용

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import {
-  Box,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Flex,
-  Heading,
-  SimpleGrid,
-  Button,
-  phoneIcon,
-  Emaillcon,
-  Table,
-  Th,
-  Td,
-  Center,
-  Circle,
-  Square,
-} from "@chakra-ui/react";
-import { PhoneIcon } from "@chakra-ui/icons";
 import React from "react";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@chakra-ui/react";
+import * as PropTypes from "prop-types";
+
+function ModalBdy(props) {
+  return null;
+}
+
+ModalBdy.propTypes = { children: PropTypes.node };
 
 function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
-      <Center bg={"red"} h={"200px"}>
-        <Square bg={"blue"} w={"100px"} h={"100px"}>
-          Leoem
-        </Square>
-      </Center>
-
-      <Center bg={"red.200"} h={"200px"}>
-        <Circle bg={"blue.200"} w={"100px"} h={"100px"}>
-          <PhoneIcon />
-        </Circle>
-      </Center>
+      <Button onClick>모달 열기</Button>
+      <Modal
+        closeOnOverlayClick={false}
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        motionPreset="slideInBottom"
+      ></Modal>
+      <Modal isOpen={onOpen}>onClose</Modal>
+      <ModalContent>
+        <ModalHeader>모달의 제목</ModalHeader>
+        <ModalBody>dlsfkldkflsdfjdklsjfljdslkfjdlsfjldsjflkjls</ModalBody>
+        <ModalFooter>
+          <Button colorScheme="blue" onClick={onClose}></Button>
+        </ModalFooter>
+      </ModalContent>
     </>
   );
 }
