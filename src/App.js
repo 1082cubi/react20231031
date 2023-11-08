40910a7a789bacafa7a2ce9eb918b37f9ea43cf7
1082cubi <1082cubi@hanmail.net>
2023-11-08 14:54:24 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,51 +1,25 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Box, Select, Spinner, Text } from "@chakra-ui/react";
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Box } from "@chakra-ui/react";
+
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Box>home page</Box>} />
+      <Route path="/path1" element={<Box>경로1</Box>} />
+      <Route path="/path2" element={<Box>경로2</Box>} />
+      <Route path="/path3" element={<Box>경로3</Box>} />
+    </>,
+  ),
+);
 
 function App(props) {
-  const [customerId, setCustomerId] = useState(0);
-  const [customerName, setCustomerName] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    axios
-      .get("/api/main1/sub4?id=" + customerId)
-      .then(({ data }) => setCustomerName(data))
-      .catch((e) => setCustomerName(null))
-      .finally(() => setIsLoading(false));
-  }, [customerId]);
-  return (
-    <div>
-      <Select
-        placeholder="고객번호 선택"
-        onChange={({ target }) => setCustomerId(target.value)}
-      >
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
-        <option value="121">121</option>
-      </Select>
-      <Box>
-        {isLoading && <Spinner />}
-        {isLoading || customerId}
-        <>
-          {customerName === null ? (
-            <Text>고객정보가 아직 없습니다</Text>
-          ) : (
-            <Text>고객 이름은 이거 : {customerName}</Text>
-          )}
-        </>
-      </Box>
-    </div>
-  );
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
